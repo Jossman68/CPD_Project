9f3b0bac239bff40b2fc515a2368221635d7335b
Frank Karae <frankjkarae@gmail.com>
2026-05-26 00:26:22 +1000

    Changed port to deploy

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import { syncAll } from './connect.js';
 
 const PORT = process.env.PORT || 8000;
 
-app.listen(PORT, async () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', async () => {
+    console.log(`Server is running on port ${PORT}`);
     await syncAll();
 });
